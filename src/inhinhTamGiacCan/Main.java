65f28a5c1ffd1cf4b0d9377ab5135652b3edfcf4
Minhkun912 <minhnguyenduc506@gmail.com>
2023-03-08 11:48:19 +0700

    first commit

---
+++ src/inhinhTamGiacCan/Main.java
@@ -0,0 +1,15 @@
+package inhinhTamGiacCan;
+
+public class Main {
+    public static void main(String[] args) {
+//        for (int a = 0; a <= 9; a++) {
+            for (int i = 7; i >= 1; i--) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(" * ");
+                }
+                System.out.print("\n");
+            }
+//            System.out.print(" ");
+//        }
+    }
+}
